783b51e6e21429313897a38b69fce1369c4e7987
RomanOstrous <ostrous.roman@gmail.com>
2024-07-31 21:43:21 +0300

    added basic files to get started

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './index.css';
+
+import App from './pages/App';
+import NotFound from './pages/NotFound';
+import Contact from './pages/Contact';
+
+const container = document.getElementById('root') as HTMLElement;
+
+createRoot(container).render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/contact/:id" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
---
+++ src/pages/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='app'>
+      qqqqqqqqqqqqqqqq
+    </div>
+  )
+}
+
+export default App
